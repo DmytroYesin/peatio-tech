cd8a9df46ad17570b7b65731abe93af0c8323c2b
dmytro <dmytroyesin@gmail.com>
2018-10-24 15:49:39 +0300

    Add Home menu section

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
       <div class="container no-override">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header page-scroll">
-          <a class="navbar-brand page-scroll" href="#main">
+          <a class="navbar-brand page-scroll" href="#home">
             <img src="../assets/img/peatio-logo.png" alt="Logo">
           </a>
           <button class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
@@ -17,6 +17,7 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse navbar-right index-nav" id="navbar-collapse">
           <ul class="nav navbar-nav">
+            <li><a class="page-scroll" href="#home">Home</a></li>
             <li><a class="page-scroll" href="#services">Services</a></li>
             <li><a class="page-scroll" href="#features">Features</a></li>
             <li><a class="page-scroll" href="#platform">Platform</a></li>
